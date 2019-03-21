b91984b8e162d06746dbf808ed0ff52dd6378908
Daniel Ebert <15155173+bearbytes@users.noreply.github.com>
2019-03-22 00:02:45 +0100

    change type order

--- src/transform/NamedTypeIR.ts
+++ src/transform/NamedTypeIR.ts
@@ -1,9 +1,7 @@
 import { GraphQLNamedType } from 'graphql'
 import { TypeIR, transformType } from './TypeIR'
 
-export type NamedTypeIR = TypeIR & {
-  typename: string
-}
+export type NamedTypeIR = { typename: string } & TypeIR
 export function transformNamedType(T: GraphQLNamedType): NamedTypeIR {
   const typename = T.name
   if (typename.startsWith('__')) return
