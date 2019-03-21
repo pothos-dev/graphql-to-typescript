7f929f802a45496622e9f97e5adfc53932804e36
Daniel Ebert <15155173+bearbytes@users.noreply.github.com>
2019-03-21 23:06:29 +0100

    add fields, prettyprint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-import { parseFromUrl } from './graphql'
-import transformSchema from './transform'
+import prettyJson from 'prettyjson'
+import { writeFileSync, readFileSync } from 'fs'
+import { GraphQLSchema } from 'graphql'
 
-parseFromUrl('httpss://countries.trevorblades.com/')
-  .then(transformSchema)
-  .then(console.log)
+export function readSchemaFile(filePath: string): GraphQLSchema {
+  return JSON.parse(readFileSync(filePath, { encoding: 'utf8' }))
+}
+
+export function writeSchemaFile(schema: GraphQLSchema) {
+  writeFileSync('schema.json', JSON.stringify(schema, null, 2))
+}
+
+console.log(prettyJson.render(readSchemaFile('schema.json')))
--- src/transform/ObjectTypeIR.ts
+++ src/transform/ObjectTypeIR.ts
@@ -1,10 +1,45 @@
-import { GraphQLObjectType } from 'graphql'
+import {
+  GraphQLObjectType,
+  GraphQLType,
+  isScalarType,
+  isObjectType,
+  isInterfaceType,
+  isUnionType,
+  isEnumType,
+  isInputObjectType,
+  isListType,
+  isNonNullType,
+} from 'graphql'
+import { map, zip, keys, values } from 'rambda'
 
 export interface ObjectTypeIR {
   kind: 'object'
+  fields: Record<string, unknown>
+  interfaces?: unknown
 }
 export function transformObjectType(T: GraphQLObjectType): ObjectTypeIR {
+  const fields = T.getFields()
+
+  const fieldsIR = zip(
+    keys(fields),
+    values(fields)
+      .map((field) => field.type)
+      .map(identifyType)
+  )
+
   return {
     kind: 'object',
+    fields: fieldsIR,
   }
+}
+
+function identifyType(T: GraphQLType): string {
+  if (isScalarType(T)) return 'Scalar'
+  if (isObjectType(T)) return 'Object'
+  if (isInterfaceType(T)) return 'Interface'
+  if (isUnionType(T)) return 'Union'
+  if (isEnumType(T)) return 'Enum'
+  if (isInputObjectType(T)) return 'InputIbject'
+  if (isListType(T)) return 'List'
+  if (isNonNullType(T)) return 'NonNull'
 }
