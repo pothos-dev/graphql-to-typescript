f09294f10db4bf148fc58ecaee87ccfcf16aefb7
Daniel Ebert <15155173+bearbytes@users.noreply.github.com>
2019-03-22 18:53:56 +0100

    extract SelectionIR

--- src/transform/OperationIR.ts
+++ src/transform/OperationIR.ts
@@ -1,14 +1,5 @@
-import {
-  OperationDefinitionNode,
-  SelectionNode,
-  FragmentSpreadNode,
-  InlineFragmentNode,
-  FieldNode,
-  SelectionSetNode,
-} from 'graphql'
-import { ObjectTypeIR } from './ObjectTypeIR'
-import { NamedTypeIR } from './NamedTypeIR'
-import { TypeIR } from './TypeIR'
+import { OperationDefinitionNode } from 'graphql'
+import { SelectionIR, transformSelection } from './SelectionIR'
 
 export interface OperationIR {
   kind: 'query' | 'mutation' | 'subscription'
@@ -22,38 +13,4 @@ export function transformOperation(T: OperationDefinitionNode) {
     name: T.name.value,
     data: T.selectionSet.selections.map(transformSelection),
   }
-}
-
-interface SelectionIR {
-  name: string
-  schemaName: string
-  selections?: SelectionIR[]
-}
-
-export function transformSelection(T: SelectionNode): SelectionIR {
-  switch (T.kind) {
-    case 'Field':
-      return transformField(T)
-    case 'FragmentSpread':
-      return transformFragmentSpread(T)
-    case 'InlineFragment':
-      return transformInlineFragment(T)
-  }
-}
-
-function transformField(T: FieldNode): SelectionIR {
-  return {
-    name: (T.alias || T.name).value,
-    schemaName: T.name.value,
-    selections:
-      T.selectionSet && T.selectionSet.selections.map(transformSelection),
-  }
-}
-
-function transformFragmentSpread(T: FragmentSpreadNode): SelectionIR {
-  throw 'not yet implemented'
-}
-
-function transformInlineFragment(T: InlineFragmentNode): SelectionIR {
-  throw 'not yet implemented'
 }
---
+++ src/transform/SelectionIR.ts
@@ -0,0 +1,40 @@
+import {
+  SelectionNode,
+  FieldNode,
+  FragmentSpreadNode,
+  InlineFragmentNode,
+} from 'graphql'
+
+export interface SelectionIR {
+  name: string
+  schemaName: string
+  selections?: SelectionIR[]
+}
+
+export function transformSelection(T: SelectionNode): SelectionIR {
+  switch (T.kind) {
+    case 'Field':
+      return transformField(T)
+    case 'FragmentSpread':
+      return transformFragmentSpread(T)
+    case 'InlineFragment':
+      return transformInlineFragment(T)
+  }
+}
+
+function transformField(T: FieldNode): SelectionIR {
+  return {
+    name: (T.alias || T.name).value,
+    schemaName: T.name.value,
+    selections:
+      T.selectionSet && T.selectionSet.selections.map(transformSelection),
+  }
+}
+
+function transformFragmentSpread(T: FragmentSpreadNode): SelectionIR {
+  throw 'not yet implemented'
+}
+
+function transformInlineFragment(T: InlineFragmentNode): SelectionIR {
+  throw 'not yet implemented'
+}
