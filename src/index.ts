7bb16a0f0670b671c9adaa6472bdf2bbeb8933be
Daniel Ebert <15155173+bearbytes@users.noreply.github.com>
2019-03-23 10:23:42 +0100

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,12 @@
-import { readFileSync, writeFileSync } from 'fs'
-import { parse, validate, buildClientSchema } from 'graphql'
-import { loadIntrospection } from './graphql/Introspection'
+import { validate } from 'graphql'
 import { loadDocument } from './graphql/Document'
 import { loadSchema } from './graphql/Schema'
 import { transformDocument } from './transform/DocumentIR'
-import { render } from 'prettyjson'
 import { generate } from './generate'
 import { transformSchema } from './transform/SchemaIR'
 import { writeFile } from 'fs-extra'
 
 main()
-
-// downloadSchema('https://api.graphloc.com/graphql').then((schema) => {
-//   const schemaIR = transformSchema(schema)
-//   writeFileSync('schemaIR.json', JSON.stringify(schemaIR, null, 2))
-//   // console.log(prettyJson.render(schemaIR))
-// })
-
-// // const schema = loadSchema('schema.json')
-// // const schemaIR = transformSchema(schema)
-// // writeFileSync('schemaIR.json', JSON.stringify(schema, null, 2))
-
-// // console.log(prettyJson.render(loadSchema('schema.json')))
-
-// const schemaIR = JSON.parse(readFileSync('schemaIR.json', { encoding: 'utf8' }))
-// writeFileSync('output.ts', generate(schemaIR))
 
 async function main() {
   try {
