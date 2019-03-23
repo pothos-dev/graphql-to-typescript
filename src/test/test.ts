d5f617c876d2f2352b10bb0039b0245eccdc126f
Daniel Ebert <15155173+bearbytes@users.noreply.github.com>
2019-03-23 11:01:56 +0100

    update snapshots

--- src/test/test.ts
+++ src/test/test.ts
@@ -3,6 +3,8 @@ import { transformSchema } from '../transform/SchemaIR'
 
 test('parse schema', async () => {
   const schema = await loadSchema('src/test/schema.gql')
+  expect(schema).toMatchSnapshot('Schema')
+
   const schemaIR = transformSchema(schema)
-  expect(schemaIR).toMatchSnapshot()
+  expect(schemaIR).toMatchSnapshot('SchemaIR')
 })
