b623369c84e02516fbe0440a2b5aa9b4a1e48016
Daniel Ebert <15155173+bearbytes@users.noreply.github.com>
2019-03-27 21:38:42 +0100

    build scripts & generate export

--- packages/graphql-to-typescript/src/index.ts
+++ packages/graphql-to-typescript/src/index.ts
@@ -1,37 +1,33 @@
 import { loadDocument } from './graphql/Document'
 import { loadSchema } from './graphql/Schema'
-import { transformDocument } from './transform/DocumentIR'
+import { transformDocument, DocumentIR } from './transform/DocumentIR'
 import { generateCode } from './generate'
-import { transformSchema } from './transform/SchemaIR'
-import { writeFile } from 'fs-extra'
+import { transformSchema, SchemaIR } from './transform/SchemaIR'
 import { validateDocument } from './graphql/Validation'
+import { GraphQLSchema } from 'graphql'
+import fs from 'fs-extra'
 
-main()
+type URL = string
+type FilePath = string
 
-async function main() {
-  try {
-    const [{ sourceCode, document }, schema] = await Promise.all([
-      loadDocument('src/test/document.gql'),
-      loadSchema('src/text/schema.gql'),
-    ])
-
-    validateDocument(document, schema)
-
-    const schemaIR = transformSchema(schema)
-    await writeFile(
-      'src/output/schemaIR.json',
-      JSON.stringify(schemaIR, null, 2)
-    )
-
-    const documentIR = transformDocument(document)
-    await writeFile(
-      'src/output/documentIR.json',
-      JSON.stringify(documentIR, null, 2)
-    )
-
-    const output = await generateCode(schemaIR, documentIR, sourceCode)
-    await writeFile('src/output/output.ts', output)
-  } catch (e) {
-    console.error('Caught exception: ' + e.message)
+export default async function generate(opts: {
+  schema: URL | FilePath | GraphQLSchema
+  document: FilePath
+  outFile?: FilePath
+}): Promise<{
+  schemaIR: SchemaIR
+  documentIR: DocumentIR
+  result: string
+}> {
+  const schema =
+    typeof opts.schema == 'object' ? opts.schema : await loadSchema(opts.schema)
+  const { sourceCode, document } = await loadDocument(opts.document)
+  validateDocument(document, schema)
+  const schemaIR = transformSchema(schema)
+  const documentIR = transformDocument(document)
+  const result = await generateCode(schemaIR, documentIR, sourceCode)
+  if (opts.outFile) {
+    await fs.writeFile(opts.outFile, result)
   }
+  return { schemaIR, documentIR, result }
 }
