cba50f1b6a810cf748ed0175f18b11d9b9649ca7
Daniel Ebert <15155173+bearbytes@users.noreply.github.com>
2019-04-16 18:19:03 +0200

    add cli to main project

---
+++ packages/graphql-to-typescript/src/cli.ts
@@ -0,0 +1,30 @@
+import program from 'commander'
+import generate from '.'
+
+program.version('0.0.0')
+
+program
+  .option(
+    '-s, --schema <uri>',
+    'Schema file, Introspection JSON file or GraphQL Endpoint'
+  )
+  .option(
+    '-d, --documents <file>',
+    'Document file (Queries, Mutations, Subscriptions, Fragments)'
+  )
+  .option('-o, --outfile <file>', 'Path of the generated file')
+  .parse(process.argv)
+
+main()
+
+async function main() {
+  try {
+    await generate({
+      schema: program.schema,
+      document: program.documents,
+      outFile: program.outfile,
+    })
+  } catch (error) {
+    console.error(error.message)
+  }
+}
