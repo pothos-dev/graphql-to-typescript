436b01536bedf7fd32de03ae4afe17e68d64f6f4
Daniel Ebert <15155173+bearbytes@users.noreply.github.com>
2019-05-15 21:23:28 +0200

    typesafe update function

--- packages/graphql-apollo/src/types.ts
+++ packages/graphql-apollo/src/types.ts
@@ -4,7 +4,12 @@ import {
   Mutation,
   OperationVariables,
 } from '@bearbytes/graphql-to-typescript'
-import { QueryOptions, ApolloQueryResult, MutationOptions } from 'apollo-client'
+import {
+  QueryOptions,
+  ApolloQueryResult,
+  MutationOptions,
+  MutationUpdaterFn,
+} from 'apollo-client'
 import { FetchResult, Observable } from 'apollo-link'
 
 export interface Client<GQL> {
@@ -28,24 +33,21 @@ export interface Client<GQL> {
 export type QueryConfig<GQL, Name extends keyof GQL> = Omit<
   QueryOptions,
   'query' | 'variables'
-> & {
-  operationName: Name
-} & ({} extends OperationVariables<GQL, Name>
-    ? { variables?: OperationVariables<GQL, Name> }
-    : { variables: OperationVariables<GQL, Name> })
+> &
+  OperationConfig<GQL, Name> &
+  TypedVariables<GQL, Name>
+
+export type MutateConfig<GQL, Name extends keyof GQL> = Omit<
+  MutationOptions,
+  'mutation' | 'variables' | 'update'
+> &
+  OperationConfig<GQL, Name> &
+  TypedVariables<GQL, Name> &
+  TypedUpdate<GQL, Name>
 
 export type QueryResult<GQL, Name extends keyof GQL> = ApolloQueryResult<
   OperationData<GQL, Name>
 >
-
-export type MutateConfig<GQL, Name extends keyof GQL> = Omit<
-  MutationOptions,
-  'mutation' | 'variables'
-> & {
-  operationName: Name
-} & ({} extends OperationVariables<GQL, Name>
-    ? { variables?: OperationVariables<GQL, Name> }
-    : { variables: OperationVariables<GQL, Name> })
 
 export type MutateResult<GQL, Name extends keyof GQL> = FetchResult<
   OperationData<GQL, Name>
@@ -54,3 +56,18 @@ export type MutateResult<GQL, Name extends keyof GQL> = FetchResult<
 // export interface SubscriptionResult<T, Name extends Subscription<T>> {}
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+
+export type OperationConfig<GQL, Name extends keyof GQL> = {
+  operationName: Name
+}
+
+export type TypedVariables<
+  GQL,
+  Name extends keyof GQL
+> = {} extends OperationVariables<GQL, Name>
+  ? { variables?: OperationVariables<GQL, Name> }
+  : { variables: OperationVariables<GQL, Name> }
+
+export type TypedUpdate<GQL, Name extends keyof GQL> = {
+  update?: MutationUpdaterFn<OperationData<GQL, Name>>
+}
